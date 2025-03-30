25dd5aa35cc5bb53302c5fada25d5cf49d36e9f2
vbeni30 <151450702+vbeni30@users.noreply.github.com>
2025-03-31 01:53:58 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -52,7 +52,7 @@ export default function Navbar() {
               alt="Rhema Church"
               width={100}
               height={50}
-              className="w-[100px] h-[100px]"
+              className="w-[100px] h-[50px]"
             />
           </Link>
 
