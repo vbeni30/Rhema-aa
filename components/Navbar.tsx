52c70b877109df0fe072b9e5e10fc6530794ff5c
vbeni30 <151450702+vbeni30@users.noreply.github.com>
2025-03-31 01:41:04 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -48,7 +48,7 @@ export default function Navbar() {
         <div className="max-w-7xl mx-auto px-6 flex justify-between items-center">
           <Link href="/" className="relative z-10">
             <Image
-              src="/Logo Rhema No Text(1).webp"
+              src="/rhema.webp"
               alt="Rhema Church"
               width={120}
               height={40}
